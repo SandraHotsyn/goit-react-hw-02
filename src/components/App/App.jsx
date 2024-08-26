6c240d823513c38542c60634988aa0ad29455410
Hotsyn <85835454+SandraHotsyn@users.noreply.github.com>
2024-08-26 08:10:43 +0300

    add components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,28 +1,38 @@
 import { useState } from "react";
 import "./App.module.css";
 import Buttons from "../Buttons/Buttons";
+import Description from "../Description/Description";
 
 export default function App() {
-  const [count, setCount] = useState(0);
+  const [clicks, setClicks] = useState(0);
+  const [values, setValues] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const updateClicks = () => {
+    setClicks(clicks + 1);
+  };
+
+  const resetClicks = () => {
+    setClicks(0);
+  };
+
+  const updateValues = () => {
+    setValues({
+      ...values,
+      good: 1,
+    });
+  };
+
   return (
     <>
-      <Buttons />
-      <h1>Sip Happens Café</h1>
-      <p>
-        Please leave your feedback about our service by selecting one of the
-        options below.
-      </p>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Description />
+      <Buttons value={clicks} onUpdate={updateClicks} />
+      <Buttons value={clicks} onUpdate={updateClicks} />
+      <button onClick={updateValues}>clicks{values.good + 1}</button>
+      <button onClick={resetClicks}>reset clicks</button>
     </>
   );
 }
--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -1,16 +1,3 @@
-export default function Buttons() {
-  const handleClick = (value) => {
-    console.log("Cliiiick", value);
-  };
-
-  const handleMouseEnter = (event) => {
-    console.log("handleMouseEnter", event);
-  };
-
-  return (
-    <div>
-      <h1 onMouseEnter={handleMouseEnter}> state React </h1>
-      <button onClick={() => handleClick(5)}>Click me</button>
-    </div>
-  );
+export default function Buttons({ value, onUpdate }) {
+  return <button onClick={onUpdate}>Click me {value}</button>;
 }
